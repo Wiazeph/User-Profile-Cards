7cf4e2a4f6e59b0e8e7fbb32879446685bf54f2d
Emre Erden <emreerden@pm.me>
2023-02-14 00:24:36 +0300

    feat: add form inputs properties to FormValidationSchema

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -1,27 +1,16 @@
 <script setup>
 import axios from "axios";
 
-import { reactive } from "vue";
-
 import { useList } from "./useList";
+
+import { ref } from "vue";
 
 import { Form, Field, ErrorMessage } from "vee-validate";
 import * as yup from "yup";
 
 const { getUser } = useList();
 
-let usersInfo = reactive({
-  name: "",
-  surname: "",
-  age: "",
-  title: "",
-  company: "",
-  email: "",
-  linkedin: "",
-  twitter: "",
-  github: "",
-  thumbnail: "",
-});
+const formRef = ref();
 
 const FormValidationSchema = yup.object({
   name: yup.string().required(),
@@ -36,46 +25,15 @@ const FormValidationSchema = yup.object({
   thumbnail: yup.string().required().url(),
 });
 
-async function onSubmit() {
-  const {
-    name,
-    surname,
-    age,
-    title,
-    company,
-    email,
-    linkedin,
-    twitter,
-    github,
-    thumbnail,
-  } = usersInfo;
-
+async function onSubmit(values) {
   await axios.post("/users.json", {
-    name,
-    surname,
-    age,
-    title,
-    company,
-    email,
-    linkedin,
-    twitter,
-    github,
-    thumbnail,
     date: new Date(),
+    ...values,
   });
 
   getUser();
 
-  usersInfo.name = "";
-  usersInfo.surname = "";
-  usersInfo.age = "";
-  usersInfo.title = "";
-  usersInfo.company = "";
-  usersInfo.email = "";
-  usersInfo.linkedin = "";
-  usersInfo.twitter = "";
-  usersInfo.github = "";
-  usersInfo.thumbnail = "";
+  formRef.value.resetForm();
 
   console.log("Data has been added successfully...");
 }
@@ -89,106 +47,78 @@ async function onSubmit() {
       <Form
         @submit="onSubmit"
         class="form-submit"
+        ref="formRef"
         :validation-schema="FormValidationSchema"
       >
         <div class="form-section-row">
           <label>Name:<span class="text-red-600">*</span></label>
-          <Field
-            name="name"
-            v-model="usersInfo.name"
-            placeholder="Name"
-            class="form-input"
-          />
+          <Field name="name" placeholder="Name" class="form-input" />
           <ErrorMessage name="name" />
         </div>
+
         <div class="form-section-row">
           <label>Surname:<span class="text-red-600">*</span></label>
-          <Field
-            name="surname"
-            v-model="usersInfo.surname"
-            placeholder="Surname"
-            class="form-input"
-          />
+          <Field name="surname" placeholder="Surname" class="form-input" />
           <ErrorMessage name="surname" />
         </div>
+
         <div class="form-section-row">
           <label>Age:<span class="text-red-600">*</span></label>
-          <Field
-            name="age"
-            v-model="usersInfo.age"
-            placeholder="Age"
-            class="form-input"
-          />
+          <Field name="age" placeholder="Age" class="form-input" />
           <ErrorMessage name="age" />
         </div>
+
         <div class="form-section-row">
-          <label>Title/Job:<span class="text-red-600">*</span></label>
-          <Field
-            name="title"
-            v-model="usersInfo.title"
-            placeholder="Title/Job"
-            class="form-input"
-          />
+          <label>Title:<span class="text-red-600">*</span></label>
+          <Field name="title" placeholder="Title" class="form-input" />
           <ErrorMessage name="title" />
         </div>
+
         <div class="form-section-row">
-          <label>Company Name:</label>
-          <Field
-            name="company"
-            v-model="usersInfo.company"
-            placeholder="Company Name"
-            class="form-input"
-          />
+          <label>Company:</label>
+          <Field name="company" placeholder="Company" class="form-input" />
           <ErrorMessage name="company" />
         </div>
+
         <div class="form-section-row">
-          <label>E-Mail<span class="text-red-600">*</span></label>
-          <Field
-            name="email"
-            v-model="usersInfo.email"
-            placeholder="E-Mail"
-            class="form-input"
-          />
+          <label>Email:<span class="text-red-600">*</span></label>
+          <Field name="email" placeholder="Email" class="form-input" />
           <ErrorMessage name="email" />
         </div>
+
         <div class="form-section-row">
-          <label>Linkedin Account:</label>
+          <label>LinkedIn Account:</label>
           <Field
             name="linkedin"
-            v-model="usersInfo.linkedin"
-            placeholder="Linkedin Account"
+            placeholder="LinkedIn Account"
             class="form-input"
           />
-          <ErrorMessage name="linkedin" z />
+          <ErrorMessage name="linkedin" />
         </div>
+
         <div class="form-section-row">
           <label>Twitter Account:</label>
           <Field
             name="twitter"
-            v-model="usersInfo.twitter"
             placeholder="Twitter Account"
             class="form-input"
           />
           <ErrorMessage name="twitter" />
         </div>
+
         <div class="form-section-row">
-          <label>GitHub Account:</label>
+          <label>Github Account:</label>
           <Field
             name="github"
-            v-model="usersInfo.github"
-            placeholder="GitHub Account"
+            placeholder="Github Account"
             class="form-input"
           />
           <ErrorMessage name="github" />
         </div>
+
         <div class="form-section-row">
-          <label>Thumbnail Link:<span class="text-red-600">*</span></label>
-          <Field
-            name="thumbnail"
-            v-model="usersInfo.thumbnail"
-            placeholder="Thumbnail Link"
-            class="form-input"
-          />
+          <label>Thumbnail:<span class="text-red-600">*</span></label>
+          <Field name="thumbnail" placeholder="Thumbnail" class="form-input" />
           <ErrorMessage name="thumbnail" />
         </div>
 
